23762a843c166a5eec05700ebbefdeefede24853
ershadul1 <ershadul.rayhan@gmail.com>
2021-04-23 18:07:51 +0600

    Add fetchWeatherData action creator

---
+++ src/Redux/actions/actionCreators.js
@@ -0,0 +1,19 @@
+import * as actions from './actionTypes';
+
+const fetchWeatherData = () => (dispath) => {
+  const {
+    REACT_APP_API_KEY,
+    REACT_APP_API_URL,
+  } = process.env;
+
+  fetch(`${REACT_APP_API_URL}?q=Munich,de&APPID=${REACT_APP_API_KEY}&cnt=40`)
+    .then((response) => response.json())
+    .then((data) => {
+      dispath({ type: actions.FETCH_DATA_SUCCESS, payload: data });
+    })
+    .catch((error) => {
+      dispath({ type: actions.FETCH_DATA_FAILURE, payload: error });
+    });
+};
+
+export default fetchWeatherData;
