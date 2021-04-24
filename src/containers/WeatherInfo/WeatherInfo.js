0fd5630463ecd080af5a35d71f1031e5f1a47fe2
ershadul1 <ershadul.rayhan@gmail.com>
2021-04-24 22:14:51 +0600

    Add UnitSwitch and Arrows componets in WeatherInfo

--- src/containers/WeatherInfo/WeatherInfo.js
+++ src/containers/WeatherInfo/WeatherInfo.js
@@ -1,3 +1,32 @@
-const WeatherInfo = () => (<p>weather info</p>);
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Container from '@material-ui/core/Container';
+import UnitSwitch from '../../components/UnitSwitch/UnitSwitch';
+import Arrows from '../../components/Arrows/Arrows';
 
-export default WeatherInfo;
+const WeatherInfo = ({ data }) => {
+  const [unit, setUnit] = useState('F');
+
+  const handleUnitChange = (value) => {
+    setUnit(value);
+  };
+
+  console.log(data);
+  return (
+    <Container maxWidth="md">
+      <UnitSwitch unit={unit} handleUnitChange={handleUnitChange} />
+      <Arrows />
+    </Container>
+  );
+};
+
+WeatherInfo.propTypes = {
+  data: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.weatherData.data,
+});
+
+export default connect(mapStateToProps, null)(WeatherInfo);
