501c2d80aad93a853c767e588b3ce9ffc3ff1b38
ershadul1 <ershadul.rayhan@gmail.com>
2021-04-26 02:32:16 +0600

    Integrate WeatherCards and Barchart in WeatherInfo

--- src/containers/WeatherInfo/WeatherInfo.js
+++ src/containers/WeatherInfo/WeatherInfo.js
@@ -4,19 +4,57 @@ import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import UnitSwitch from '../../components/UnitSwitch/UnitSwitch';
 import Arrows from '../../components/Arrows/Arrows';
+import WeatherCards from '../../components/WeatherCards/WeatherCards';
+import BarChart from '../../components/BarChart/BarChart';
 
 const WeatherInfo = ({ data }) => {
   const [unit, setUnit] = useState('F');
+  const [currentCard, setCurrentCard] = useState(0);
+  const totalCards = 5;
+  const { hourlyData } = data;
+  const { dailyData } = data;
+  const tempList = hourlyData[dailyData[currentCard][0]].temp;
 
   const handleUnitChange = (value) => {
     setUnit(value);
   };
 
-  console.log(data);
+  const moveToNextCard = () => {
+    setCurrentCard((currentCard) => {
+      if (currentCard < totalCards - 1) {
+        return currentCard + 1;
+      }
+      return currentCard;
+    });
+  };
+
+  const moveToPrevCard = () => {
+    setCurrentCard((currentCard) => {
+      if (currentCard > 0) {
+        return currentCard - 1;
+      }
+      return currentCard;
+    });
+  };
+
   return (
     <Container maxWidth="md">
       <UnitSwitch unit={unit} handleUnitChange={handleUnitChange} />
-      <Arrows />
+      <Arrows
+        next={moveToNextCard}
+        prev={moveToPrevCard}
+        currentCard={currentCard}
+        totalCards={totalCards}
+      />
+      <WeatherCards
+        currentCard={currentCard}
+        dailyData={dailyData}
+        unit={unit}
+      />
+      <BarChart
+        tempList={tempList}
+        unit={unit}
+      />
     </Container>
   );
 };
