40c6a1d84ce86b243ebf871a97366d16f85b8c86
ershadul1 <ershadul.rayhan@gmail.com>
2021-04-25 23:37:59 +0600

    Add functionalities in Arrows

--- src/components/Arrows/Arrows.js
+++ src/components/Arrows/Arrows.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import LeftArrow from './left.svg';
@@ -13,28 +13,49 @@ const useStyles = makeStyles(() => ({
     margin: '1vh auto',
     maxWidth: '750px',
   },
-  disabled: {
-    display: 'none',
-  },
   arrow: {
     display: 'block',
     width: '120px',
     height: '60px',
     margin: '1vw',
   },
+  disabled: {
+    display: 'none',
+  },
 }));
 
-export default function Arrows() {
+export default function Arrows({
+  next, prev, currentCard, totalCards,
+}) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Button>
-        <img src={LeftArrow} alt="left-arrow" className={classes.arrow} />
-      </Button>
-      <Button>
-        <img src={RightArrow} alt="right-arrow" className={classes.arrow} />
-      </Button>
+      <div>
+        <Button onClick={prev} className={`${currentCard === 0 ? classes.disabled : ''}`}>
+          <img
+            src={LeftArrow}
+            alt="left-arrow"
+            className={classes.arrow}
+          />
+        </Button>
+      </div>
+      <div>
+        <Button onClick={next} className={`${currentCard === totalCards - 1 ? classes.disabled : ''}`}>
+          <img
+            src={RightArrow}
+            alt="right-arrow"
+            className={classes.arrow}
+          />
+        </Button>
+      </div>
     </div>
   );
 }
+
+Arrows.propTypes = {
+  next: PropTypes.func.isRequired,
+  prev: PropTypes.func.isRequired,
+  currentCard: PropTypes.number.isRequired,
+  totalCards: PropTypes.number.isRequired,
+};
